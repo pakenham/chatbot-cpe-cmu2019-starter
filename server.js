0ab72d7a8b1062b06f1694cfff384a6fb06d0c6e
pakenham <pakenham98@gmail.com>
2019-02-02 14:38:59 +0700

    reply message edited

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const client = new Client(config)
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
-  
 })
-
 // to do here
 app.post('/webhook', middleware(config), (req, res) => {
   const event = req.body.events[0];  // received message
@@ -22,10 +20,42 @@ app.post('/webhook', middleware(config), (req, res) => {
   if (event.type === 'message') {
     const message = event.message;  // store received message
     console.log(message)
-    client.replyMessage(event.replyToken, { // reply fn
-      type: 'text',
-      text: message.type 
-    });
+    client.replyMessage(event.replyToken, 
+      {
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+            "type": "buttons",
+            "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "Menu",
+            "text": "Please select",
+            "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://google.com/"
+            },
+            "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "message",
+                  "label": "Add to cart",
+                  "text": "no no no"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com"
+                }
+            ]
+        }
+      })
   }
 })
 
