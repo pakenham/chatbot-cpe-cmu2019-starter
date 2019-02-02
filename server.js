1604fe7df7f12f4a9f418737a4fde53cce69ce7f
pakenham <pakenham98@gmail.com>
2019-02-02 10:59:00 +0700

    middleware/token  added

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
+const middleware = require('@line/bot-sdk').middleware
 const app = express()
+
+const config = {  // get from  dev.line  channel
+  channelAccessToken: '036kzdBV0KYG34YGdNgK7z67uRBAyO9lFC4+fZZUIVBFxnmL95edZKw0kbqfxSbafOBTgd4U6Mfm8hrKhtd6xsflRBUGY5l8ESd6ROn0BaIE3bg4JMlK2CjsnxGxGXQW6wJHAhJiu793HXdZl2P7eAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'e6f48551c7fce914aa23a62bbab1b948'
+}
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
+})
+
+app.post('/webhook', middleware(config), (req, res) => {
+  //req.body.events // webhook event objects
+  //req.body.destination // user ID of the bot (optional)
+  console.log('webhook success')
 })
 
 app.set('port', (process.env.PORT || 4000))
