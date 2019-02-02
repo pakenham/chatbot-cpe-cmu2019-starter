3d99fd1bd6b181b97ff280c28cb38cc8740bd09a
pakenham <pakenham98@gmail.com>
2019-02-02 11:41:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/webhook', middleware(config), (req, res) => {
-  //req.body.events // webhook event objects
-  //req.body.destination // user ID of the bot (optional)
-  console.log('webhook success')
+  res.send('asdasd')
 })
 
 app.set('port', (process.env.PORT || 4000))
